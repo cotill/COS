59392957b269582cf82190471d98d5246c0921f9
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2024-12-11 14:23:57 -0700

    update theme settings in RootLayout switched to forced dark theme

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -29,8 +29,9 @@ export default function RootLayout({
       <body className="bg-background text-foreground">
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          // defaultTheme="system"
+          // enableSystem
+          forcedTheme="dark"
           disableTransitionOnChange
         >
           {" "}
