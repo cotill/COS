53ea8dcf612fb78155c3992c081301d072ac1d0a
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2024-12-15 17:29:13 -0700

    Refactor project page; extract subheading into its own component

--- frontend/app/Employee/Projects/page.tsx
+++ frontend/app/Employee/Projects/page.tsx
@@ -8,7 +8,10 @@ export default async function projectpage(){
   // cosnt allProjects = await getProject();
     return (
       // <ProjectsList initialProjects={allProjects}/>
-
+      <>
+      // make the suheading its own componet and pass it the name
+      <h2> This is the project page</h2>
       <ProjectsList />
+      </>
     );
 }
