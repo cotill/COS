dd7dc16d7a3f29be2aacbba0da323fc1cb6720e3
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2025-01-30 00:59:16 -0700

    feat: add react-datepicker dependency

--- frontend/components/employeeComponents/DateRangePicker.tsx
+++ frontend/components/employeeComponents/DateRangePicker.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import DatePicker from 'react-datepicker';
-// import "react-datepicker/dist/react-datepicker.css";
+import DatePicker from 'react-datepicker'; //npm install react-datepicker --save
 import "@/components/employeeComponents/DateRangePicker.css";
 
 interface DateRangePickerProps {
