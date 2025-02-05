20ef36b7db43d5c7f4865904158d02527fba7919
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2025-02-05 13:46:31 -0700

    Refactor: removed duplicate code

--- frontend/app/student_applications/createPdf.jsx
+++ frontend/app/student_applications/createPdf.jsx
@@ -1,50 +1,16 @@
 "use client";
-import React from "react";
-import { Document, Page, Text, View, StyleSheet, PDFDownloadLink } from "@react-pdf/renderer";
-import ReactPDF from "@react-pdf/renderer";
-
-// Create styles
-const styles = StyleSheet.create({
-  page: {
-    flexDirection: "row",
-    backgroundColor: "#E4E4E4",
-  },
-  section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1,
-  },
-});
-
-// Create Document Component
-const MyDocument = () => (
-  <Document>
-    <Page size="A4" style={styles.page}>
-      <View style={styles.section}>
-        <Text>Section #1</Text>
-      </View>
-      <View style={styles.section}>
-        <Text>Section #2</Text>
-      </View>
-    </Page>
-  </Document>
-);
-
-function CreatePdf() {
-  return (
-    <div className="App">
-      <PDFDownloadLink document={<MyDocument />} fileName="somename.pdf">
-        {({ blob, url, loading, error }) => (loading ? "Loading document..." : "Download now!")}
-      </PDFDownloadLink>
-    </div>
-  );
-}
-export default CreatePdf;
-
-"use client";
 
 import React, { useState } from "react";
-import { Document, Page, Text, View, StyleSheet, PDFDownloadLink, Image, Svg } from "@react-pdf/renderer";
+import {
+  Document,
+  Page,
+  Text,
+  View,
+  StyleSheet,
+  PDFDownloadLink,
+  Image,
+  Svg,
+} from "@react-pdf/renderer";
 import { Project } from "@/utils/types";
 import { Button } from "@/components/ui/button";
 
@@ -54,8 +20,8 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     paddingTop: 50,
     paddingBottom: 50,
-    paddingLeft: 50,   
-    paddingRight: 50,  
+    paddingLeft: 50,
+    paddingRight: 50,
   },
   section: {
     marginBottom: 0,
@@ -70,16 +36,15 @@ const styles = StyleSheet.create({
     marginBottom: 4,
   },
   image: {
-    width: 100, 
-    height: 40, 
+    width: 100,
+    height: 40,
   },
   header: {
-    flexDirection: "row",  // Arrange items in a row
-    justifyContent: "space-between",  // Push items to the sides
-    alignItems: "center",  // Align vertically in the center
+    flexDirection: "row", // Arrange items in a row
+    justifyContent: "space-between", // Push items to the sides
+    alignItems: "center", // Align vertically in the center
     marginBottom: 10,
   },
-
 });
 
 const MyDocument = ({ project }) => (
@@ -103,20 +68,21 @@ function CreatePdf({ project }) {
   if (!project) {
     return <p>Failed to load project.</p>;
   }
-  
+
   return (
     <div className="App">
-      <PDFDownloadLink document={<MyDocument project={project} />} fileName={`Capstone-${project.title}.pdf`}>
-          {/* {({ blob, url, loading, error }) => (loading ? "Loading document..." : "Download now!")} */}
-          {({ blob, url, loading, error }) => (
-            <Button className="bg-black hover:bg-slate-600">
-              {loading ? "Loading document..." : "Download now!"}
-            </Button>
-          )}
+      <PDFDownloadLink
+        document={<MyDocument project={project} />}
+        fileName={`Capstone-${project.title}.pdf`}
+      >
+        {/* {({ blob, url, loading, error }) => (loading ? "Loading document..." : "Download now!")} */}
+        {({ blob, url, loading, error }) => (
+          <Button className="bg-black hover:bg-slate-600">
+            {loading ? "Loading document..." : "Download now!"}
+          </Button>
+        )}
       </PDFDownloadLink>
     </div>
   );
 }
 export default CreatePdf;
-
-
