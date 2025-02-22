3132a1c0c15a6d1abf2aeda080899eb644418eff
T-Smithy <tanis.smith@ucalgary.ca>
2025-02-22 03:37:24 -0700

    gettin info

--- frontend/app/Student/Tasks/page.tsx
+++ frontend/app/Student/Tasks/page.tsx
@@ -1,14 +1,47 @@
 import Headingbar from "@/components/employeeComponents/Headingbar";
+import UnauthorizedPage from "@/components/unAuthorized";
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
 
-export default function taskspage(){
+export default async function taskspage() {
+  const supabase = await createClient();
+
+  // Get authenticated user
+  const { data: userInfo, error: authError } = await supabase.auth.getUser();
+  if (!userInfo.user || authError) {
+    redirect("/sign-in");
+  }
+
+  // Fetch student info
+  const { data: studentInfo, error: stuError } = await supabase
+  .from("Students")
+  .select(
+    `
+    *,
+    Teams:team_id (
+      *,
+      Projects:project_id (*)
+    )
+  `
+  )
+  .eq("student_id", userInfo.user.id)
+  .single();
+
+  console.log(studentInfo)
+
+  if (!studentInfo || stuError) {
+    return <UnauthorizedPage />;
+  }
 
   return (
     <>
-      <Headingbar
-        text='Student Tasks'
-      />
+      <Headingbar text="Student Tasks" />
       <div>
-        <span>This is the student tasks page</span>
+        <h2>Student Information</h2>
+        <p>Name: {studentInfo.full_name}</p>
+        <p>Email: {studentInfo.email}</p>
+        <p>Major: {studentInfo.major}</p>
+
       </div>
     </>
   );
