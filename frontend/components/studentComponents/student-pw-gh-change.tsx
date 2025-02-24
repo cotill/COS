219ca4a8382008e9a25d05a321f04bfac57691f6
Michele Pham <michele_pham@hotmail.ca>
2025-02-24 11:27:57 -0700

    button changes

--- frontend/components/studentComponents/student-pw-gh-change.tsx
+++ frontend/components/studentComponents/student-pw-gh-change.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Eye, EyeOff } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { createClient } from "@/utils/supabase/client"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
@@ -16,18 +16,44 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { userInfo } from "os";
+
+interface StudentChangesProps {
+  userId: string
+}
 
 
 
-
-
-const StudentChanges = () => {
+const StudentChanges = ({ userId }: StudentChangesProps) => {
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [githubUser, setGithubUser] = useState("");
   const [isVisible, setIsVisible] = useState(false);
   
   const toggleVisibility = () => setIsVisible(prevState => !prevState);
   const router = useRouter();
+
+
+  const supabase = createClient();
+
+  useEffect(() => {
+    // This will fetch data when the component mounts
+    const fetchUserData = async () => {
+        const { data: userInfo, error: userError } = await supabase
+            .from("Students")
+            .select("github")
+            .eq("student_id", userId)  // match userid with the employee id
+            .single(); // only 1 record returned
+
+        if (userError) {
+            console.error("error getting user data:", userError);
+        } else {
+            setGithubUser(userInfo.github);
+        }
+    };
+
+    fetchUserData(); 
+  }, [userId]);
 
   const handleCancel = () => {
     setNewPassword("");
@@ -88,12 +114,12 @@ const StudentChanges = () => {
                   <Input
                     id="new-password"
                     type={isVisible ? "text" : "password"}
-                    className="border border-gray-300 rounded px-4 py-2 text-black w-full pr-10"
+                    className="border border-gray-300 rounded px-4 py-2 text-white w-full pr-10"
                     value={newPassword}
                     onChange={(e) => setNewPassword(e.target.value)}
                   />
                   <Button
-                    className="absolute inset-y-0 right-0 flex items-center px-3 text-black focus:outline-none"
+                    className="absolute inset-y-0 right-0 flex items-center px-3 text-white focus:outline-none"
                     type="button"
                     onClick={toggleVisibility}
                     aria-label={isVisible ? "Hide password" : "Show password"}
@@ -103,13 +129,31 @@ const StudentChanges = () => {
                 </div>
               </div>
               <div className="space-y-1">
-                <Label htmlFor="new">Confirm Password</Label>
-                <Input id="new" type="password" />
+                <Label htmlFor="comfirm-password">Confirm Password</Label>
+                <div className="relative">
+                <Input
+                    id="confirm-password"
+                    type={isVisible ? "text" : "password"}
+                    className="border border-gray-300 rounded px-4 py-2 text-white w-full pr-10"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                  />
+                  <Button
+                    className="absolute inset-y-0 right-0 flex items-center px-3 text-white focus:outline-none"
+                    type="button"
+                    onClick={toggleVisibility}
+                    aria-label={isVisible ? "Hide password" : "Show password"}
+                  >
+                    {isVisible ? <EyeOff size={20} aria-hidden="true" /> : <Eye size={20} aria-hidden="true" />}
+                  </Button>
+                  </div>
               </div>
             </CardContent>
             <CardFooter>
-              <Button onClick={() => handlePasswordReset}>Save</Button>
-              <Button onClick={() => handleCancel}>Cancel</Button>
+              <div className="flex justify-end gap-4">
+                <Button className="bg-[#81C26C] text-white px-8 py-2 mb-6 rounded hover:bg-green-600" onClick={() => handlePasswordReset}>Save</Button>
+                <Button className="bg-[#E75973] text-white px-8 py-2 mb-6 rounded hover:bg-red-600" onClick={() => handleCancel}>Cancel</Button>
+              </div>
             </CardFooter>
           </Card>
         </TabsContent>
@@ -123,16 +167,13 @@ const StudentChanges = () => {
             </CardHeader>
             <CardContent className="space-y-2">
               <div className="space-y-1">
-                <Label htmlFor="name">Name</Label>
-                <Input id="name" defaultValue="Pedro Duarte" />
-              </div>
-              <div className="space-y-1">
-                <Label htmlFor="username">Username</Label>
-                <Input id="username" defaultValue="@peduarte" />
+                <Label htmlFor="username">GitHub Username</Label>
+                <Input id="username" defaultValue={githubUser} />
               </div>
             </CardContent>
             <CardFooter>
-              <Button>Save changes</Button>
+              <Button className="bg-[#E75973] text-white px-8 py-2 mb-6 rounded hover:bg-red-600">Save</Button>
+              <Button className="bg-[#81C26C] text-white px-8 py-2 mb-6 rounded hover:bg-green-600">Cancel</Button>
             </CardFooter>
           </Card>
         </TabsContent>
